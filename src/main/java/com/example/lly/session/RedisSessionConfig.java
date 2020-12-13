cd8a7ce818dbad57f6b268fe01765bbcafdcfd46
liluyang1999 <liluyang1999@qq.com>
2020-12-13 16:06:10 +0800

    Third Commit

--- src/main/java/com/example/lly/session/RedisSessionConfig.java
+++ src/main/java/com/example/lly/session/RedisSessionConfig.java
@@ -1,4 +1,4 @@
-package com.example.lly.Session;
+package com.example.lly.session;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
